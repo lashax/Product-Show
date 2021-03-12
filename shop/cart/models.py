25b0de164ef568c049dde7a70c20db2b2e84d2b5
Lasha Sherozia <repeop0@gmail.com>
2021-03-12 18:46:07 +0400

    Created models for cart app.

--- shop/cart/models.py
+++ shop/cart/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from shop.product.models import Product
+
+
+class Cart(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order = models.PositiveIntegerField()  # @TODO implement
+    active = models.BooleanField(default=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,
+                             related_name='items')
