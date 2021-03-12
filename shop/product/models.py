0a1b23116e477b3d881e1f9adf707c07aa623ab2
Lasha Sherozia <repeop0@gmail.com>
2021-03-12 18:31:07 +0400

    Created models for product app.

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    order = models.PositiveIntegerField()  # @TODO implement
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    is_published = models.BooleanField(default=True)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product)
